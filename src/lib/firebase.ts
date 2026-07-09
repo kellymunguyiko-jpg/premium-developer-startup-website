b297e430b31047b58a8fa90b07e6749433e5fead
kellymunguyiko-jpg <kellymunguyiko@gmail.com>
2026-07-09 21:00:31 +0000

    fix firebase config duplicate

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,8 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+console.log("Firebase API:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
